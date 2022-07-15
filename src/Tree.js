e16f2677f2744a5d9e2585574cf179235451501b
webLinsaIntel <intelligence920507@gmail.com>
2022-07-15 10:01:47 -0500

    initial commit

--- src/Tree.js
+++ src/Tree.js
@@ -23,8 +23,8 @@ export default function Tree({ data, tempData, i, setChanger, collapse_upgrade, 
             }
             if (data.children.length == 0)
                 leafCollpase(maindata);
-           
-           // expand_upgrade(maindata, tempData.id);
+
+            // expand_upgrade(maindata, tempData.id);
         }
 
         setNodeData(tempData)
@@ -32,11 +32,11 @@ export default function Tree({ data, tempData, i, setChanger, collapse_upgrade, 
     let depth = 10;
 
     return (
-         <div className={style}>
-     <div style={{ marginLeft: 30 + depth * i }}>
-            <img src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfkAxEHAxdDtXmfAAABCElEQVRIx+3QvSsEcBgH8M95SXTiyksUnVIMlIHBYJDBYjIZlMFq9QfcblEWi81ikUHJIIMuBsqRJESUiCQkb5cFeTn3trrv9nyfPv1eyCUX2DKatR0VY0RcJCseETcCg55NCGSEA8a8Gv4YBzyZlJcBH/di6GvV58G0grR4vimP+n/W3W7NKUrJC82415to1eXGvOKkvMisOz1/rdtdWlb6Jy+x6FpnshPaXFhRlnAXtORKR6o3Nju1ruJXH7LqTGvKP0LYgR2137oqm441psOh3p5ddZ9zjW2HGtLlUC3m6P3EsP1fN0ojIWvOtGhyYkNlphzKRZ07F1WeDYegBQuC2fJc/k/eAHCZOZhYMvqPAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIwLTAzLTE3VDA3OjAzOjIzKzAwOjAwMBxz1gAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMC0wMy0xN1QwNzowMzoyMyswMDowMEFBy2oAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAAAElFTkSuQmCC"} style={{ transform: "scaleX(1)", cursor: "pointer" }} onClick={() => toggle(i, setChanger)}></img>
-            <span>{tempData.name}</span>
+        <div className={style}>
+            <div style={{ marginLeft: 30 + depth * i }}>
+                <img src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfkAxEHAxdDtXmfAAABCElEQVRIx+3QvSsEcBgH8M95SXTiyksUnVIMlIHBYJDBYjIZlMFq9QfcblEWi81ikUHJIIMuBsqRJESUiCQkb5cFeTn3trrv9nyfPv1eyCUX2DKatR0VY0RcJCseETcCg55NCGSEA8a8Gv4YBzyZlJcBH/di6GvV58G0grR4vimP+n/W3W7NKUrJC82415to1eXGvOKkvMisOz1/rdtdWlb6Jy+x6FpnshPaXFhRlnAXtORKR6o3Nju1ruJXH7LqTGvKP0LYgR2137oqm441psOh3p5ddZ9zjW2HGtLlUC3m6P3EsP1fN0ojIWvOtGhyYkNlphzKRZ07F1WeDYegBQuC2fJc/k/eAHCZOZhYMvqPAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIwLTAzLTE3VDA3OjAzOjIzKzAwOjAwMBxz1gAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMC0wMy0xN1QwNzowMzoyMyswMDowMEFBy2oAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAAAElFTkSuQmCC"} style={{ transform: "scaleX(1)", cursor: "pointer" }} onClick={() => toggle(i, setChanger)}></img>
+                <span>{tempData.name}</span>
+            </div>
         </div>
-         </div>
     );
 }
